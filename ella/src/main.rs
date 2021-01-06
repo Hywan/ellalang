f5a42efe8d04ea8aa5661e38ef79d2c0bf6b77c0
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-01-06 11:55:46 -0800

    feat: allow compiling a file

--- ella/src/main.rs
+++ ella/src/main.rs
@@ -8,7 +8,7 @@ use std::io::{self, Write};
 
 mod builtin_functions;
 
-fn main() {
+fn repl() {
     let mut stdout = io::stdout();
     let stdin = io::stdin();
 
@@ -72,6 +72,68 @@ fn main() {
                     vm.restore_stack(initial_stack);
                 }
             }
+        }
+    }
+}
+
+fn interpret_file_contents(source: &str) {
+    let builtin_vars = {
+        let mut builtin_vars = BuiltinVars::new();
+        builtin_vars.add_native_fn("print", &builtin_functions::print, 1);
+        builtin_vars.add_native_fn("println", &builtin_functions::println, 1);
+        builtin_vars.add_native_fn("assert_eq", &builtin_functions::assert_eq, 2);
+        builtin_vars.add_native_fn("assert", &builtin_functions::assert, 1);
+        builtin_vars.add_native_fn("clock", &builtin_functions::clock, 0);
+        builtin_vars
+    };
+
+    let resolved_symbols = {
+        let dummy_source = "".into();
+        let mut resolver = Resolver::new(&dummy_source);
+        resolver.resolve_builtin_vars(&builtin_vars);
+        resolver.into_resolved_symbols()
+    };
+
+    let mut vm = Vm::new(&builtin_vars);
+    let mut resolved_symbol_table = &HashMap::new();
+    let mut codegen = Codegen::new("<global>".to_string(), &resolved_symbol_table);
+    codegen.codegen_builtin_vars(&builtin_vars);
+    vm.interpret(codegen.into_inner_chunk()); // load built in functions into memory
+
+    let source = source.into();
+    let mut parser = Parser::new(&source);
+    let mut ast = parser.parse_program();
+
+    let mut resolver = Resolver::new_with_existing_symbols(&source, resolved_symbols.clone());
+    resolver.resolve_top_level(&mut ast);
+    resolved_symbol_table = resolver.resolved_symbol_table();
+
+    if !source.has_no_errors() {
+        eprintln!("{}", source.errors);
+    } else {
+        let mut codegen = Codegen::new("<global>".to_string(), resolved_symbol_table);
+
+        codegen.codegen_function(&mut ast);
+
+        let chunk = codegen.into_inner_chunk();
+        match vm.interpret(chunk) {
+            InterpretResult::Ok => {}
+            InterpretResult::RuntimeError { message, line } => {
+                eprintln!("Runtime Error: {} at line {}", message, line);
+            }
+        }
+    }
+}
+
+fn main() {
+    if std::env::args().len() < 2 {
+        repl();
+    } else {
+        let path = std::env::args().nth(1).unwrap();
+        let contents = std::fs::read_to_string(path);
+        match contents {
+            Ok(contents) => interpret_file_contents(&contents),
+            Err(err) => eprintln!("Error: {}", err),
         }
     }
 }
